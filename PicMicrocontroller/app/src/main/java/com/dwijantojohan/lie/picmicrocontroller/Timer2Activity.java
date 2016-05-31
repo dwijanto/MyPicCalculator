64fe2fd0977388f4164fd757f604f6146fd5b35f
dwijanto <dwijanto@yahoo.com>
2016-05-31 11:25:27 +0800

    test commit android studio

--- PicMicrocontroller/app/src/main/java/com/dwijantojohan/lie/picmicrocontroller/Timer2Activity.java
+++ PicMicrocontroller/app/src/main/java/com/dwijantojohan/lie/picmicrocontroller/Timer2Activity.java
@@ -4,11 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Timer2Activity extends AppCompatActivity {
+    private final int prescallerValue = 1;
+    private final int clockSourceValue = 4;
+    private final int MaxPreload = 256;
 
+    private CalculatorContextWrapper ccw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer0);//using Activity_timer0 as a shared template
+
+        initialized();
+
+    }
+
+    private void initialized(){
         setTitle(R.string.TitleTimer2Activity);
-        setContentView(R.layout.activity_timer2);
+        ccw = new CalculatorContextWrapper(this,MaxPreload);
+        listener();
+        ccw.calculateTimer(prescallerValue,clockSourceValue);
+    }
+
+    private void listener(){
+        ccw.editText2.setOnFocusChangeListener(ccw);
+        ccw.editText3.setOnFocusChangeListener(ccw);
+        ccw.checkBox.setOnCheckedChangeListener(ccw);
+        ccw.spinner2.setOnItemSelectedListener(ccw);
+        ccw.spinner3.setOnItemSelectedListener(ccw);
     }
 }
